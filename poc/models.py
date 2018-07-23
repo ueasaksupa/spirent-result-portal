894a0f75eb57665ebbf9d70a6ee76b50202d1e6a
ueasaksupa <bossid1999@gmail.com>
2018-07-23 22:52:52 +0700

    part 4

---
+++ poc/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+import datetime
+# Create your models here.
+
+class Flows(models.Model):
+    pub_date = models.DateTimeField('date published')
+    test_set = models.IntegerField(default=0)
+    flow_name = models.CharField(max_length=200)
+    tx = models.IntegerField(default=0)
+    rx = models.IntegerField(default=0)
+    drop_count = models.IntegerField(default=0)
+    comments = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.flow_name+', drop: '+str(self.drop_count)
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
---
+++ poc/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return render(request, 'polls/index.html', context)
+
+
+def simple_upload(request):
+    if request.method == 'POST' and request.FILES['myfile']:
+        myfile = request.FILES['myfile']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'core/simple_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
+    return render(request, 'core/simple_upload.html')
