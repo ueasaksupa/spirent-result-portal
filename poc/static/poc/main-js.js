625db1984b6735b40ad9da073a2550211c1c24d6
ueasaksupa <bossid1999@gmail.com>
2018-07-31 01:36:25 +0700

    add export button

--- poc/static/poc/main-js.js
+++ poc/static/poc/main-js.js
@@ -26,12 +26,20 @@ $(document).ready(function() {
         fixedHeader: true,
         pageLength: 100,
         autoWidth: true,
-        columnDefs: [ {
-            targets: [ 6 ],
-            orderData: [ 6, 4 ]
-            }
+        columnDefs: [ 
+            { targets: [ 6 ], orderData: [ 6, 4 ] }
         ],
+        dom: '<"float-right export-btn-grp mr-2"B>lfrtip',
+        buttons: {
+            buttons: [
+                        { extend: 'excelHtml5', text: 'Excel', className: 'btn btn-sm btn-outline-primary'},
+                        { extend: 'pdfHtml5', text: 'PDF', className: 'btn btn-sm btn-outline-primary'},
+                        { extend: 'csvHtml5', text: 'CSV', className: 'btn btn-sm btn-outline-primary'}
+                    ],
+        },
         initComplete: function () {
+            $(".dt-buttons").attr('class', 'btn-group');
+            $('.dt-button').removeClass('dt-button')
             this.api().columns().every( function () {
                 if (this[0][0] != 0) {
                     var column = this;
@@ -41,12 +49,10 @@ $(document).ready(function() {
                             var val = $.fn.dataTable.util.escapeRegex(
                                 $(this).val()
                             );
-    
                             column
                                 .search( val ? '^'+val+'$' : '', true, false )
                                 .draw();
                         } );
-    
                     column.data().unique().sort().each( function ( d, j ) {
                         select.append( '<option value="'+d+'">'+d+'</option>' )
                     } );
