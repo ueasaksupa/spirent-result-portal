82eedde659b640e17337e6754ad5c455e9e945c4
ueasaksupa <bossid1999@gmail.com>
2018-08-02 02:30:29 +0700

    add filter

--- poc/static/poc/main-js.js
+++ poc/static/poc/main-js.js
@@ -19,6 +19,28 @@ $(document).ready(function () {
                 }
             ],
         },
+        initComplete: function () {
+            $(".dt-buttons").attr('class', 'btn-group');
+            $('.dt-button').removeClass('dt-button')
+            this.api().columns().every(function () {
+                if (this[0][0] != 0) {
+                    var column = this;
+                    var select = $('<select><option value=""></option></select>')
+                        .appendTo($(column.footer()).empty())
+                        .on('change', function () {
+                            var val = $.fn.dataTable.util.escapeRegex(
+                                $(this).val()
+                            );
+                            column
+                                .search(val ? '^' + val + '$' : '', true, false)
+                                .draw();
+                        });
+                    column.data().unique().sort().each(function (d, j) {
+                        select.append('<option value="' + d + '">' + d + '</option>')
+                    });
+                }
+            });
+        },
     });
 
     $('#table_alltestcases').DataTable({
