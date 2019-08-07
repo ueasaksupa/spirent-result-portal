dd6f67c578ac4e17159afd8fc7d916beccd9e8c1
ueasaksupa <bossid1999@gmail.com>
2019-08-07 15:09:47 +0700

    add setting.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,26 +103,6 @@ except ImportError:
 	import pymysql
 	pymysql.install_as_MySQLdb()
 
-# [START db_setup]
-# if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine'):
-#     # Running on production App Engine, so connect to Google Cloud SQL using
-#     # the unix socket at /cloudsql/<your-cloudsql-connection string>
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.mysql',
-#             'HOST': '/cloudsql/poc-ais-2018:asia-southeast1:poc-ais-2018',
-#             'NAME': 'poc',
-#             'USER': 'root',
-#             'PASSWORD': 'P@ssw0rd!',
-#         }
-#     }
-# else:
-	# Running locally so connect to either a local MySQL instance or connect to
-	# Cloud SQL via the proxy. To start the proxy via command line:
-	#
-	#     $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306
-	#
-	# See https://cloud.google.com/sql/docs/mysql-connect-proxy
 DATABASES = {
 	'default': {
 		'ENGINE': 'django.db.backends.mysql',
